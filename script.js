5bd6a6f1e980f066dca7fc1f1cf61e9801eae9a9
AleksMala <o.malaszuk@gmail.com>
2018-02-21 20:27:45 +0100

    js change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://limitless-eyrie-10600.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://limitless-eyrie-10600.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://limitless-eyrie-10600.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://limitless-eyrie-10600.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
